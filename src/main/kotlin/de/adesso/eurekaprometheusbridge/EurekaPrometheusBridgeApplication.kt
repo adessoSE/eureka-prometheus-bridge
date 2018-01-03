cec237c1122e68e80b91ad106650aca9eb1bba68
SilasMahler <simah002@stud.fh-dortmund.de>
2018-01-03 09:23:09 +0100

    optimize imports

--- src/main/kotlin/de/adesso/eurekaprometheusbridge/EurekaPrometheusBridgeApplication.kt
+++ src/main/kotlin/de/adesso/eurekaprometheusbridge/EurekaPrometheusBridgeApplication.kt
@@ -1,27 +1,14 @@
 package de.adesso.eurekaprometheusbridge
 
-import com.google.gson.JsonArray
 import khttp.get
-import org.json.JSONArray
-import org.json.JSONObject
+import org.json.XML
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.cloud.client.ServiceInstance
-import org.springframework.cloud.client.discovery.DiscoveryClient
-import org.springframework.stereotype.Component
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-import org.json.XML
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.io.File
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder.json
-
-
 
 
 @SpringBootApplication
