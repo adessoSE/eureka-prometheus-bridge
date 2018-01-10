f7a4a9c45927781e5fa7045f005ab101c3f107fd
Silas Mahler <silas-mahler@t-online.de>
2018-01-10 16:06:51 +0100

    Update EurekaPrometheusBridgeApplication.kt
    removed doubled annotation (contained in @springbootapplication

--- src/main/kotlin/de/adesso/eurekaprometheusbridge/EurekaPrometheusBridgeApplication.kt
+++ src/main/kotlin/de/adesso/eurekaprometheusbridge/EurekaPrometheusBridgeApplication.kt
@@ -8,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
-@EnableAutoConfiguration
 class EurekaPrometheusBridgeApplication
 
 fun main(args: Array<String>) {
