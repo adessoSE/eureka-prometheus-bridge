1a880635da1ee14f7aede8f19650ea35c520f342
SilasMahler <simah002@stud.fh-dortmund.de>
2018-02-13 11:08:41 +0100

    add test for configTemplates

--- src/test/kotlin/de/adesso/eurekaprometheusbridge/EurekaPrometheusBridgeApplicationTests.kt
+++ src/test/kotlin/de/adesso/eurekaprometheusbridge/EurekaPrometheusBridgeApplicationTests.kt
@@ -1,5 +1,6 @@
 package de.adesso.eurekaprometheusbridge
 
+import junit.framework.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
@@ -9,8 +10,22 @@ import org.springframework.test.context.junit4.SpringRunner
 @SpringBootTest
 class EurekaPrometheusBridgeApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun loadConfigTemplates() {
+        var eureka_config = EurekaProperties.configTemplate.reify()
+        var prometheus_config = PrometheusProperties.configTemplate.reify()
+
+        assertEquals(8761, eureka_config.valueOf(EurekaProperties.port))
+        assertEquals("http://127.0.0.1", eureka_config.valueOf(EurekaProperties.host))
+        assertEquals("/eureka/apps/", eureka_config.valueOf(EurekaProperties.apiPath))
+        assertEquals(false, eureka_config.valueOf(EurekaProperties.showJson))
+
+        assertEquals(15, prometheus_config.valueOf(PrometheusProperties.scrapeInterval))
+        assertEquals(10, prometheus_config.valueOf(PrometheusProperties.scrapeTimeout))
+        assertEquals("/eureka/apps/", prometheus_config.valueOf(PrometheusProperties.metricsPath))
+        assertEquals("http", prometheus_config.valueOf(PrometheusProperties.scheme))
+        assertEquals("src/main/resources/prometheus-basic.yml", prometheus_config.valueOf(PrometheusProperties.configFileTemplatePath))
+        assertEquals("generated-prometheus-configs/prometheus.yml", prometheus_config.valueOf(PrometheusProperties.generatedConfigFilePath))
+    }
 
 }
