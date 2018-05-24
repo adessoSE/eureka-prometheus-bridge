5b53bb3d38f96e0bbb8c1cb90b960e1245335761
SilasMahler <simah002@stud.fh-dortmund.de>
2018-05-24 11:39:52 +0200

    remove import

--- src/main/kotlin/de/adesso/eurekaprometheusbridge/ScheduledJob.kt
+++ src/main/kotlin/de/adesso/eurekaprometheusbridge/ScheduledJob.kt
@@ -1,6 +1,5 @@
 package de.adesso.eurekaprometheusbridge
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
