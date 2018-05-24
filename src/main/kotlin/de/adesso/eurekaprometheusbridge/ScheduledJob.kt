ffdf0b69953e3f3c4e044bbb442b2f36c93c91f1
SilasMahler <simah002@stud.fh-dortmund.de>
2018-05-24 11:37:55 +0200

    remove outdated annotation

--- src/main/kotlin/de/adesso/eurekaprometheusbridge/ScheduledJob.kt
+++ src/main/kotlin/de/adesso/eurekaprometheusbridge/ScheduledJob.kt
@@ -5,8 +5,7 @@ import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
 @Service
-class ScheduledJob(
-        @Autowired var eurekaQuery: EurekaQuery) {
+class ScheduledJob(var eurekaQuery: EurekaQuery) {
     
     @Scheduled(fixedDelayString = "\${query.interval.seconds:60}000")
     fun executeBridge() {
